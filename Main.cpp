7c8139b87381fa2a9384cb66f71bdedbe463571a
toazthom <109962138+toazthom@users.noreply.github.com>
2023-12-20 11:53:27 -0500

    Created main cpp file and a pre-compiled header file.

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file Main.cpp
+ * @author Thomas Toaz
+ *
+ *
+ * Main entry point for program
+ */
+
+#include "pch.h"
+//#include "CanadianExperienceApp.h"
+
+wxIMPLEMENT_APP();
---
+++ pch.h
@@ -0,0 +1,16 @@
+/**
+ * @file pch.h
+ * @author Toazt
+ *
+ *
+ */
+
+#ifndef PAIRED_COMPARISON_PROGRAM__PCH_H
+#define PAIRED_COMPARISON_PROGRAM__PCH_H
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#endif //PAIRED_COMPARISON_PROGRAM__PCH_H
